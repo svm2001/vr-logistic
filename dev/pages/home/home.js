7e4f352a632d26bba9ce21bb8a849f829be6f513
Вячеслав Сидоров <v.sidorov@intaro.email>
2025-04-16 11:35:31 +0400

    fix

--- dev/pages/home/home.js
+++ dev/pages/home/home.js
@@ -1,8 +1,10 @@
 import './home.scss'
 import main from '@/assets/scripts/main'
 import { countups } from '@/assets/scripts/modules/countups'
+import { globe } from '@/assets/scripts/modules/globe'
 
 window.onload = () => {
   main()
   countups()
+  globe()
 }
