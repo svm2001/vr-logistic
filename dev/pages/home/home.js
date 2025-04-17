6a046cc7c1eefaf2891d3a78d8bf13b757edf459
Вячеслав Сидоров <v.sidorov@intaro.email>
2025-04-17 18:51:06 +0400

    fix

--- dev/pages/home/home.js
+++ dev/pages/home/home.js
@@ -3,10 +3,26 @@ import main from '@/assets/scripts/main'
 import { countups } from '@/assets/scripts/modules/countups'
 import { globe } from '@/assets/scripts/modules/globe'
 import { video } from '@/assets/scripts/modules/video'
+import { tabs } from '@/assets/scripts/modules/tabs'
+import select from '@/assets/scripts/modules/select'
 
 window.onload = () => {
   main()
   countups()
   globe()
   video()
+  select()
+
+  if(window.innerWidth > 767) {
+    tabs({
+      tabParentSelector: '.routes',
+      tabHeaderSelector: '.routes__tabs-item',
+      tabHeaderActiveClass: 'active',
+      tabItemSelector: '.routes__content-item',
+      tabItemActiveClass: 'active',
+      event: 'click'
+    })
+  } else {
+
+  }
 }
