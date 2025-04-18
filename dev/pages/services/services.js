cf93cec4268acce73e21f01d5386668eae97b21c
Вячеслав Сидоров <v.sidorov@intaro.email>
2025-04-19 02:28:58 +0400

    services-detail

--- dev/pages/services/services.js
+++ dev/pages/services/services.js
@@ -1,8 +1,5 @@
 import './services.scss'
 import main from '@/assets/scripts/main'
-import { countups } from '@/assets/scripts/modules/countups'
-import { globe } from '@/assets/scripts/modules/globe'
-import { video } from '@/assets/scripts/modules/video'
 import { tabs } from '@/assets/scripts/modules/tabs'
 import select from '@/assets/scripts/modules/select'
 import { standartsSlider } from "@/assets/scripts/modules/standartsSlider.js"
