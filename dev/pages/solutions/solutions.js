6d5713c42e5def70944f6a408c24a858cd5912b7
Dmitry Shirin <d.shirin@stat4market.com>
2025-04-22 01:04:49 +0300

    fix страница экспертиза

--- dev/pages/solutions/solutions.js
+++ dev/pages/solutions/solutions.js
@@ -1,12 +1,12 @@
 import './solutions.scss'
 import main from '@/assets/scripts/main'
-import { tabs } from '@/assets/scripts/modules/tabs'
 import select from '@/assets/scripts/modules/select'
 import { standartsSlider } from "@/assets/scripts/modules/standartsSlider.js"
+import { feedbackSlider } from '@/assets/scripts/modules/feedbackSlider.js'
 
 window.onload = () => {
   main()
   select()
   standartsSlider()
-
+  feedbackSlider()
 }
