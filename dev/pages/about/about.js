8734b555ddfee00a5637bac33e378ad332ee68e8
Dmitry Shirin <d.shirin@stat4market.com>
2025-04-21 15:02:01 +0300

    feat страница экспертиза\о компании\контакты

---
+++ dev/pages/about/about.js
@@ -0,0 +1,10 @@
+import './about.scss'
+import main from '@/assets/scripts/main'
+import { tabs } from '@/assets/scripts/modules/tabs'
+import select from '@/assets/scripts/modules/select'
+import { standartsSlider } from "@/assets/scripts/modules/standartsSlider.js"
+
+window.onload = () => {
+  main()
+  select()
+}
