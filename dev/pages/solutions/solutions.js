37a34bd0b985fbe0a2094531a843febfb8d41dcd
Dmitry Shirin <d.shirin@stat4market.com>
2025-04-24 11:23:01 +0300

    feat страница о компании

--- dev/pages/solutions/solutions.js
+++ dev/pages/solutions/solutions.js
@@ -3,10 +3,12 @@ import main from '@/assets/scripts/main'
 import select from '@/assets/scripts/modules/select'
 import { standartsSlider } from "@/assets/scripts/modules/standartsSlider.js"
 import { feedbackSlider } from '@/assets/scripts/modules/feedbackSlider.js'
+import { initGallery } from '@/assets/scripts/modules/gallery.js'
 
 window.onload = () => {
   main()
   select()
   standartsSlider()
   feedbackSlider()
+  initGallery()
 }
