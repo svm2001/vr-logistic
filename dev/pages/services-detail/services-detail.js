d17d97ac209de46629e70f9d8feef068396c4e25
Mihail Irt <shegaasher@gmail.com>
2025-05-13 11:00:19 +0300

    fix правки

--- dev/pages/services-detail/services-detail.js
+++ dev/pages/services-detail/services-detail.js
@@ -1,8 +1,10 @@
 import './services-detail.scss'
 import main from '@/assets/scripts/main'
 import { standartsSlider } from "@/assets/scripts/modules/standartsSlider.js"
+import { dropDown } from "@/assets/scripts/modules/dropDown.js"
 
 window.onload = () => {
   main()
   standartsSlider()
+  dropDown()
 }
