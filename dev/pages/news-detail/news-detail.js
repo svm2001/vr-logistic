bfd0b7705af85f2af99efc5d19ddc5c3e225a0ca
Mihail Irt <shegaasher@gmail.com>
2025-05-15 12:07:59 +0300

    fix правки

--- dev/pages/news-detail/news-detail.js
+++ dev/pages/news-detail/news-detail.js
@@ -2,8 +2,10 @@ import './news-detail.scss'
 import main from '@/assets/scripts/main'
 import { newsSlider } from '@/assets/scripts/modules/newsSlider'
 import { video } from '@/assets/scripts/modules/video'
+import { initSocialField } from '@/assets/scripts/modules/social-field'
 window.onload = () => {
     main()
     newsSlider()
     video()
+    initSocialField()
 }
